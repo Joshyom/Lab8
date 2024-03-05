6d8d628ff7d1976c571040606cdc048d963e4844
Dang Le <nguyenh1@ualberta.ca>
2024-03-05 16:10:36 -0700

    Checking has city correct

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -70,7 +70,7 @@ public class CustomList extends ArrayAdapter<City> {
      * @param city
      */
     public boolean hasCity(City city) {
-        return false;
+        return cities.contains(city);
     }
 
     /**
